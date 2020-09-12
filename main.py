a87824b4ec11789766df2bbb6dcf1ad46acb2bf9
Damien Singh <damiensingh17@gmail.com>
2020-09-12 00:52:24 +0000

    Perfect Binary Tree and Traversals

--- main.py
+++ main.py
@@ -4,26 +4,77 @@ class BinaryTreeNode:
     self.data = None
     self.left = left
     self.right = right
-  
+
   # Inorder: Left Subtree -> Root -> Right Subtree
   def print_inorder_traversal(self, node):
     if node:
       self.print_inorder_traversal(node.left)
       print(node.data)
       self.print_inorder_traversal(node.right)
-      
+  
+  # Preorder: Root -> Left -> Right  
+  def print_preorder_traversal(self, node):
+    if node:
+      print(node.data)
+      self.print_preorder_traversal(node.left)
+      self.print_preorder_traversal(node.right)
 
-# Root
-n1 = BinaryTreeNode(1)
-# left child
-n2 = BinaryTreeNode(2)
-# right child 
-n3 = BinaryTreeNode(3)
-n1.left = n2
-n1.right = n3
+  # Postorder: Left -> Right -> Root
+  def print_postorder_traversal(self, node):
+    if node:
+      self.print_postorder_traversal(node.left)
+      self.print_postorder_traversal(node.right)
+      print(node.data)
 
 
-n1.data = 20
-n2.data = 5
-n3.data = 15
+
+
+
+
+def main():
+  # Root
+  n1 = BinaryTreeNode(1)
+  # left child
+  n2 = BinaryTreeNode(2)
+  # right child 
+  n3 = BinaryTreeNode(3)
+  n1.left = n2
+  n1.right = n3
+
+  n1.data = 20
+  n2.data = 5
+  n3.data = 15
+
+  n4 = BinaryTreeNode(4)
+  n5 = BinaryTreeNode(5)
+  n6 = BinaryTreeNode(6)
+  n7 = BinaryTreeNode(7)
+
+  n4.data = 10
+  n5.data = 12
+  n6.data = 14
+  n7.data = 18
+
+
+  n2.left = n4
+  n2.right = n5
+  n3.left = n6
+  n3.right = n7
+
+  print("Inorder Traversal : " , "\n")   
+  n1.print_inorder_traversal(n1)
+  print()
+  print("Preorder Traversal : ", "\n") 
+  n1.print_preorder_traversal(n1)
+  print()
+  print("Postorder Traversal : ", "\n") 
+  n1.print_postorder_traversal(n1)
+  print()
+
+if __name__== '__main__':
+  main()
+  
+
+
+
 
