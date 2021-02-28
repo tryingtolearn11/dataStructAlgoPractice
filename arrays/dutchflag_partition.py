cc2185d08a663986fe1a3a900e67a131e30960a8
tryingtolearn11 <damiensingh17@gmail.com>
2021-02-28 14:52:56 -0500

    dutch

--- arrays/dutchflag_partition.py
+++ arrays/dutchflag_partition.py
@@ -22,4 +22,31 @@ def dutch_flag_bruteForce(pivot_index: int, A: List[int]) -> List[int]:
 
 
 
-print(dutch_flag_bruteForce(3, A=[5, 4, 1, 2, 2]))
+# print(dutch_flag_bruteForce(3, A=[5, 4, 1, 2, 2]))
+
+
+
+
+
+
+
+def dutch_flag_opt1(pivot_index: int, A: List[int]) -> List[int]:
+    pivot = A[pivot_index]
+    smaller = 0
+    
+    for i in range(len(A)):
+        if A[i] < pivot:
+            A[i], A[smaller] = A[smaller], A[i]
+            smaller+=1
+    
+    print(A)
+
+    larger = len(A)-1
+    for i in reversed(range(len(A))):
+        if A[i] > pivot:
+            A[i], A[larger] = A[larger], A[i]
+            larger-=1
+
+    return A
+
+print(dutch_flag_opt1(2, A=[5, 6, 3, 1, 4]))
