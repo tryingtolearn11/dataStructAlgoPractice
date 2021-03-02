494dd655b55f3a8d77b4637b23c77bcccf200c3b
tryingtolearn11 <damiensingh17@gmail.com>
2021-03-01 20:41:55 -0500

    invariant_three

--- arrays/dutch_invariants.py
+++ arrays/dutch_invariants.py
@@ -15,7 +15,7 @@ A = [3, 1, 2, 2, 1, 3, 2, 3]
 # Optimized:
 # O(n) time complexity and space complexity? O(?) -> O(1) 
 # Solved in 2 passes. BONUS: Can we solve in one pass and keep O(n) time?
-def invariantOne(A: List[int]) -> List[int]:
+def invariant_one(A: List[int]) -> List[int]:
     i = 0
     for j in range(i+1, len(A)):
         if A[j] == A[i]:
@@ -37,10 +37,25 @@ def invariantOne(A: List[int]) -> List[int]:
 
     return A
 
-
-
-# print(invariantOne(A))
-
+# print(invariant_one(A))
 
 
 
+# Invariant Three
+# Given array A of n objects, with boolean valued keys, reorder so that the key
+# false appears first. Use O(1) additional space and O(n) time.
+
+
+def invariant_three(B: List[bool]) -> List[bool]:
+    j = 0
+    for i in range(j+1, len(B)):
+        if B[i] == False:
+            B[i], B[j]  = B[j], B[i]
+            i, j = i+1, j+1
+        else:
+            i+=1
+
+    return B
+
+print(invariant_three(B=[1, 0, 1, 0, 0, 1]))
+print(invariant_three(B=[1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1]))
