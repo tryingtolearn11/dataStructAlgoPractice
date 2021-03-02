64effb0528ead70f49daac17f16040024ab60e2d
tryingtolearn11 <damiensingh17@gmail.com>
2021-03-01 22:38:22 -0500

    5.6

--- arrays/5.6/buy_and_sell_stock_once.py
+++ arrays/5.6/buy_and_sell_stock_once.py
@@ -3,9 +3,9 @@ from typing import List
 
 
 
-
 # Brute Force
-
+# Time Complexity: O(n**2) 
+# Space Complexity: O(1)?
 def buy_sell_once(A: List[float]) -> float:
     d, max_d = 0, 0
     for i in range(len(A)):
@@ -20,5 +20,13 @@ def buy_sell_once(A: List[float]) -> float:
 A = [310, 315, 275, 295, 260, 270, 290, 230, 255, 250]
 print(buy_sell_once(A))
 
+def buy_sell_once_optimized(A: List[float]) -> float:
+    min_price_so_far, max_profit = float('inf'), 0.0
+    for price in A:
+        max_profit_sell_today = price - min_price_so_far
+        max_profit = max(max_profit, max_profit_sell_today)
+        min_price_so_far = min(min_price_so_far, price)
+    return max_profit
 
-
+print("Optimized :")
+print(buy_sell_once_optimized(A))
